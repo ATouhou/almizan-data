16065090dc3dead1d46352afb82dd8d9de2b719d
Alireza <alireza@Alireza-Ubuntu.(none)>
2012-12-05 19:23:01 +0330

    add section extractor

---
+++ sections.py
@@ -0,0 +1,38 @@
+""" starts with aya.py output """
+
+import codecs, sys
+from pyquery import PyQuery as pq
+
+almizan = codecs.open('data/almizan.html', encoding='utf-8').read()
+errors = open('data/errors.txt', 'w')
+
+d = pq(almizan)
+for i, div in enumerate(d('div')):
+	div = pq(div)
+	ayas = div.find('.aye')
+	if ayas:
+		rels = [pq(p).attr('rel') for p in ayas]
+	else:
+		errors.write('empty div\n')
+		continue
+
+	sura, aya = [], []
+	for rel in rels:
+		s, a = rel.split('-')
+		sura.append(int(s))
+		aya.append(int(a))
+
+	mi, ma = min(aya), max(aya)
+
+	if len(set(sura)) != 1 or aya != range(mi, ma+1):
+		errors.write(str(rels) + '\n')
+		continue
+
+	sura = sura.pop()
+	div.attr('rel', '%d-%d:%d' % (sura, mi, ma))
+
+	if i % 50 == 0: sys.stdout.write('.'); sys.stdout.flush()
+
+d.root.write('data/output.html', encoding='utf-8')
+
+print ' Section tags added'
