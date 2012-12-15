97c8f93f20054c2f44a9faf633a530a9b8981358
Alireza Nourian <alireza.nournia@gmail.com>
2012-12-15 19:15:52 +0330

    replace extra length character

--- refine.py
+++ refine.py
@@ -12,18 +12,17 @@ for line in codecs.open('data/raw.md', encoding='utf8'):
 	if line.startswith('>'):
 		line = line + '\n'
 		if len(line) < 5: line = ''
-		
+
 	if line.find(u'تفسیر المیزان') >= 0:
 		print line,
 		line = ''
-		
+
 	if line.startswith('# '):
 		print line,
-		
-		
+
 	# refine characters
-	line = line.replace(u'ي', u'ی').replace(u'ك', u'ک')
-	
+	line = line.replace(u'ي', u'ی').replace(u'ك', u'ک').replace(u'ـ', '')
+
 	# refine punctuations
 	line = re.sub(ur' ([:،؟!\.\)])', r'\1', line)
 
